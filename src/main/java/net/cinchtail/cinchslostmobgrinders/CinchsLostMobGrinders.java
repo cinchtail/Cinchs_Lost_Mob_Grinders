ea7d69b4c58fb0eae715c68196b056681401589e
Joshua Davies <cinchtail@gmail.com>
2026-05-13 14:54:36 -0400

    Initial commit

---
+++ src/main/java/net/cinchtail/cinchslostmobgrinders/CinchsLostMobGrinders.java
@@ -0,0 +1,8 @@
+package net.cinchtail.cinchslostmobgrinders;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CinchsLostMobGrinders.MODID)
+public class CinchsLostMobGrinders {
+    public static final String MODID = "cinchslostmobgrinders";
+}
